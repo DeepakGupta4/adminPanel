2381f5e95bd088d0be1a45720a49a291df7f71ed
DeepakGupta4 <dkguptak9369.com>
2025-03-24 09:33:25 +0530

    cccc

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/admin/",  // 👈 Ensure this is set if your app starts from /admin
-  server: {
-    port: 5174, // Ya jo bhi port use kar rahe ho
-  },
+  base: "/admin/", // ✅ Fix: Correct base for Vercel deployment
 });
